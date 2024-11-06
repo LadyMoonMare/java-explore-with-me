989cc5d462a7920aa7a5eba85cc5f207bfef0ab8
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-11-06 22:53:51 +0300

    add user schema to main service

---
+++ ewm/src/main/java/ru/yandex/practicum/user/model/User.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.user.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "app_users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "name")
+    private String name;
+}
